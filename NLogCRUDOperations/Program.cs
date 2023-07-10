fdf87b799c0f3668ebe9282d94886bfa983da756
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-10 15:00:06 +0530

    [Rajeshwari kota] Adding Dictionary CRUD Operations

--- NLogCRUDOperations/Program.cs
+++ NLogCRUDOperations/Program.cs
@@ -11,7 +11,22 @@ namespace NLogCRUDOperations
         static void Main(string[] args)
         {
             Console.WriteLine("....NLOG-CRUD OPERATIONS....");
-            ArrayListOperations.ArrayListCRUD();
+            Console.WriteLine("Enter the Option the below");
+            Console.WriteLine("\n1:ArrayListCrudOperations\n2:DictionaryCrudOperations\n22:Default");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    ArrayListOperations.ArrayListCRUD();
+                    break;
+                case 2:
+                    DictionaryOperations.DictionaryCRUD();
+                    break;
+                default:
+                    Console.WriteLine("Enter the Above Options");
+                    break;
+            }
             Console.ReadLine();
         }
     }
