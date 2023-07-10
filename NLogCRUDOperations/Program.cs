979985782ec389fd02f327c5dce32310e629bfa2
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-10 14:26:53 +0530

    [Rajeshwari kota] Adding Nlog-CRUD Operations

---
+++ NLogCRUDOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogCRUDOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....NLOG-CRUD OPERATIONS....");
+            Console.ReadLine();
+        }
+    }
+}
